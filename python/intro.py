6dd80405485cd7223d78817aef3be7f1eff827d7
DraganM03 <draganmijatovic.s1a@gmail.com>
2025-09-26 08:31:00 +0200

    python if else and loops

--- python/intro.py
+++ python/intro.py
@@ -1,3 +1,25 @@
-n = 25
+# intro to python 
+
+# ----- if else -----
+n = 25                      # deklaracija je atipicna
 if n % 3 == 0:
-    pass
+    print("n = 3k")
+elif n % 3 == 1:
+    print("n = 3k + 1")
+else:
+    print("n = 3k + 2")
+
+# ----- loops -----
+
+# --- while ---
+i = 0
+while i < 10:
+    print(i, end=" ")       # end=" " da ne bi prelazilo u novi red
+    i += 1
+
+print()                    # prelazak u novi red
+
+# --- for ---
+for i in range(10):         # ponasa se kao for each u drugim jezicima
+    print(i, end=" ")
+
