441b3a3b53e1b709e23ce381e5d998bdfae9f954
DraganM03 <draganmijatovic.s1a@gmail.com>
2025-09-26 08:44:00 +0200

    fixed the comments

--- python/intro.py
+++ python/intro.py
@@ -1,7 +1,7 @@
 # intro to python 
 
 # ----- if else -----
-n = 25                      # deklaracija je atipicna
+n = 25                          # deklaracija je atipicna
 if n % 3 == 0:
     print("n = 3k")
 elif n % 3 == 1:
@@ -14,21 +14,21 @@ else:
 # --- while ---
 i = 0
 while i < 10:
-    print(i, end=" ")       # end=" " da ne bi prelazilo u novi red
+    print(i, end=" ")           # end=" " da ne bi prelazilo u novi red
     i += 1
 
-print()                    # prelazak u novi red
+print()                         # prelazak u novi red
 
 # --- for ---
-for i in range(10):         # ponasa se kao for each u drugim jezicima
+for i in range(10):             # ponasa se kao for each u drugim jezicima
     print(i, end=" ")
 
-print()                    # prelazak u novi red
+print()                         # prelazak u novi red
 
 # ----- functions -----
 def kvadrati_brojeva(a, b):
-    for i in range(a, b+1):
+    for i in range(a, b+1, 1):     # +1 da bi ukljucio i b, 3. argument je korak (moze biti i negativan)
         print(i**2, end=" ")
 
 kvadrati_brojeva(2, 8)
-print()                    # prelazak u novi red
+print()                         # prelazak u novi red
