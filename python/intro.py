1f541333141755c0e0f9347fc2cc97b3db31711b
DraganM03 <draganmijatovic.s1a@gmail.com>
2025-09-26 09:00:20 +0200

    standardne operacije nad listama

--- python/intro.py
+++ python/intro.py
@@ -1,7 +1,7 @@
 # intro to python 
 
 # ----- if else -----
-n = 25                          # deklaracija je atipicna
+n = 25                          # deklaracija ne zahtijeva navodjenje tipa promjenljive
 if n % 3 == 0:
     print("n = 3k")
 elif n % 3 == 1:
@@ -32,3 +32,28 @@ def kvadrati_brojeva(a, b):
 
 kvadrati_brojeva(2, 8)
 print()                         # prelazak u novi red
+
+
+# ----- data structures -----
+# --- list ---
+# - ponasa se kao dinamicki niz, jer nije bas kao ulancana lista
+
+list = [5, 8, 10, 13, 24, 30, 48]  # deklaracija liste, ne moraju svi podaci da budu istog tipa
+
+# - standardne operacije nad listama -
+print(list[0], list[4], list[6])   # pristup elementu liste (indeksiranje pocinje od 0)
+list[4] = 25                       # promjena vrijednosti elementa liste
+print(list)                        # ispis cijele liste
+
+
+# --- tuple ---
+
+
+# --- dictionary ---            # ekivalenta hash mapama u drugim jezicima
+
+
+# --- set ---
+
+
+# --- 3rd party data structures ---
+# counter, queue, heap, ...
